787c26534977d0a33a13fd9b096febf85e606794
pinstripeninjas <pinstripeninjas@gmail.com>
2020-09-06 15:35:00 -0700

    initial commit

---
+++ src/script.js
@@ -0,0 +1,70 @@
+const green = "green";
+const yellow = "yellow";
+const orange = "orange";
+const red = "red";
+
+const map = L.map("map", {
+	center: [32.5, -111.2],
+	zoom: 8,
+	maxZoom: 8,
+	minZoom: 8,
+	attributionControl: false,
+	zoomControl: false,
+	dragging: false,
+	doubleClickZoom: false,
+});
+
+const basemap = L.tileLayer(
+	"https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png"
+).addTo(map);
+
+const pima = L.geoJSON.ajax("../geojson/pima.geojson", { style: startingColor }).addTo(map);
+const pinal = L.geoJSON.ajax("../geojson/pinal.geojson", { style: startingColor }).addTo(map);
+const santaCruz = L.geoJSON
+	.ajax("../geojson/santa-cruz.geojson", { style: startingColor })
+	.addTo(map);
+const graham = L.geoJSON.ajax("../geojson/graham.geojson", { style: startingColor }).addTo(map);
+const greenlee = L.geoJSON.ajax("../geojson/greenlee.geojson", { style: startingColor }).addTo(map);
+const cochise = L.geoJSON.ajax("../geojson/cochise.geojson", { style: startingColor }).addTo(map);
+
+function startingColor() {
+	return { color: green };
+}
+
+pima.on("click", function (e) {
+	let currentColor = e.layer.options.color;
+	pima.setStyle(cycleColor(currentColor));
+});
+pinal.on("click", function (e) {
+	let currentColor = e.layer.options.color;
+	pinal.setStyle(cycleColor(currentColor));
+});
+santaCruz.on("click", function (e) {
+	let currentColor = e.layer.options.color;
+	santaCruz.setStyle(cycleColor(currentColor));
+});
+graham.on("click", function (e) {
+	let currentColor = e.layer.options.color;
+	graham.setStyle(cycleColor(currentColor));
+});
+greenlee.on("click", function (e) {
+	let currentColor = e.layer.options.color;
+	greenlee.setStyle(cycleColor(currentColor));
+});
+cochise.on("click", function (e) {
+	let currentColor = e.layer.options.color;
+	cochise.setStyle(cycleColor(currentColor));
+});
+
+function cycleColor(currentColor) {
+	switch (currentColor) {
+		case green:
+			return { color: yellow };
+		case yellow:
+			return { color: orange };
+		case orange:
+			return { color: red };
+		case red:
+			return { color: green };
+	}
+}
